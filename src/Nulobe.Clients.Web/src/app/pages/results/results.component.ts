07bd6cbb781a0c3a6a61a78a866160108430129c
Michael Fry-White <michaelfry2002@gmail.com>
2017-08-03 23:25:38 +0200

    Load facts from server, animate spinner smartly

--- src/Nulobe.Clients.Web/src/app/pages/results/results.component.ts
+++ src/Nulobe.Clients.Web/src/app/pages/results/results.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, BehaviorSubject } from 'rxjs';
+
+import { FactApiClient, Fact } from '../../features/api/api.swagger';
+
+import { ResultsPathHelper } from './results-path.helper';
 
 @Component({
   selector: 'app-results',
@@ -7,9 +13,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsComponent implements OnInit {
 
-  constructor() { }
+  private _loading: BehaviorSubject<boolean> = new BehaviorSubject(true);
+
+  private facts$: Observable<Fact[]>;
+  private loading$: Observable<boolean> = this._loading.asObservable();
+
+  constructor(
+    private factApiClient: FactApiClient,
+    private router: Router
+  ) { }
 
   ngOnInit() {
-  }
+    let { url } = this.router;
+    let pathSections = url.split('/');
+    let tagsString = pathSections[pathSections.length - 1];
 
+    let tags = ResultsPathHelper.decode(tagsString);
+    if (!tags.length) {
+      throw new Error('error parsing tags');
+    }
+
+    this.facts$ = this.factApiClient.list(tags.join(','));
+
+    // Delay emitting loading = false intelligently:
+    let delay = Observable.timer(500);
+    Observable.combineLatest([this.facts$, delay])
+      .subscribe(() => this._loading.next(false));
+  }
 }
