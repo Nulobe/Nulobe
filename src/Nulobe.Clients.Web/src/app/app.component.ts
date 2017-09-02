879665adc430174d1ce75edbd70d2b645933d927
Michael Fry-White <michaelfry2002@gmail.com>
2017-09-02 16:43:51 +1200

    Add link to home

--- src/Nulobe.Clients.Web/src/app/app.component.ts
+++ src/Nulobe.Clients.Web/src/app/app.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FactApiClient } from './core/api';
+import { Router, NavigationEnd } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
+  private isHome$: Observable<boolean>;
 
   constructor(
-    private router: Router,
-    private factApiClient: FactApiClient
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -23,5 +22,12 @@ export class AppComponent implements OnInit {
     this.router.events.subscribe(x => {
       console.log(x);
     });
+
+    this.isHome$ = this.router.events
+      .filter(e => e instanceof NavigationEnd)
+      .map((e: NavigationEnd) => {
+        let urlSplit = e.url.split('/');
+        return urlSplit.length === 1 || urlSplit[1] === '';
+      });
   }
 }
