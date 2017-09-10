5a362690fdb3cf03a0f53ec9cbaf2eecbcd189a0
Michael Fry <michaelfry2002@gmail.com>
2017-09-10 14:07:42 +1200

    FIx some stuff

--- src/Nulobe.Clients.Web.Host/Pre-Build.ps1
+++ src/Nulobe.Clients.Web.Host/Pre-Build.ps1
@@ -2,9 +2,9 @@
 $projectDir = $args[1]
 $nswagExePath = $args[2]
 
-# $solutionDir = 'C:\Users\Michael\Source\Repos\VeganFacts'
-# $projectDir = 'C:\Users\Michael\Source\Repos\VeganFacts\src\Nulobe.Clients.Web.Host'
-# $nswagExePath = 'C:\Users\Michael\.nuget\packages\nswag.msbuild\11.3.3\build\NSwag.exe'
+$solutionDir = 'C:\Source2\Nulobe'
+$projectDir = 'C:\Source2\Nulobe\src\Nulobe.Clients.Web.Host'
+$nswagExePath = 'C:\Users\Michael Fry-White\.nuget\packages\nswag.msbuild\11.3.3\build\NSwag.exe'
 
 # Execute Copy-Files.ps1
 $copyFilesScriptPath = [io.path]::Combine($solutionDir, 'Copy-Files.ps1')
@@ -13,4 +13,4 @@ Invoke-Expression "& `"$copyFilesScriptPath`" $copyFilesScriptArgs"
 
 # Execute NSwag
 $nswagExeArgs = "swagger2tsclient /input:`"$projectDir\..\Nulobe.Api\swagger.json`" /output:`"$projectDir/../Nulobe.Clients.Web/src/app/core/api/api.swagger.ts`" /className:`"{controller}Client`" /typescriptVersion:2.0 /template:`"Angular`" /promiseType:`"Promise`" /dateTimeType:`"Date`" /generateClientInterfaces:true /generateDtoTypes:true /typeStyle:`"Interface`""
-Start-Process -NoNewWindow -PassThru -Wait -FilePath $nswagExePath -ArgumentList $nswagExeArgs
+Start-Process -NoNewWindow -PassThru -Wait -FilePath `"$nswagExePath`" -ArgumentList $nswagExeArgs
